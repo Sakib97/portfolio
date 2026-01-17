4a98a2d15d5e8532b8f549f802475e81edd69200
Sakib97 <ashekseum86@gmail.com>
2026-01-17 19:33:16 +0600

    feat: Refactor NavigationBar and HomePage components for improved styling and functionality

--- src/components/ui/NavigationBar.jsx
+++ src/components/ui/NavigationBar.jsx
@@ -1,44 +1,59 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import useScrollDirection from '../../hooks/useScrollDirection';
-import { useState } from 'react';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import useScrollDirection from "../../hooks/useScrollDirection";
+import { useState } from "react";
 // import styles from "../../features/styles/NavigationBar.module.css";
 import styles from "../styles/Navbar.module.css";
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import { Link } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link, NavLink } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 
 const NavigationBar = () => {
-    // Offcanvas state
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  // Offcanvas state
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const showNavbar = useScrollDirection();
-    return (
-        <div>
-            <Navbar bg="light" expand="lg"
-                // className="shadow-sm mb-3"
-                className={`shadow-sm fixed-top ${showNavbar ? styles.navbarShow : styles.navbarHide}`}>
-                <Container>
-                    <Navbar.Brand as={Link} to="/">Ashek Seum</Navbar.Brand>
+  const showNavbar = useScrollDirection();
+  return (
+    <div>
+      <Navbar
+        bg="light"
+        expand="lg"
+        // className="shadow-sm mb-3"
+        className={`shadow-sm fixed-top ${
+          showNavbar ? styles.navbarShow : styles.navbarHide
+        }`}
+      >
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Ashek Seum
+          </Navbar.Brand>
 
-                    <Navbar.Toggle onClick={handleShow} aria-controls="offcanvasNavbar-expand-lg" />
-                    <Navbar.Offcanvas
-                        id="offcanvasNavbar-expand-lg"
-                        aria-labelledby="offcanvasNavbarLabel-expand-lg"
-                        placement="end"
-                        show={show}
-                        onHide={handleClose}
-                    >
-                        <Offcanvas.Header closeButton onHide={handleClose} className="justify-content-center">
-                            <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
-                                Menu
-                            </Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <Nav style={{ fontSize: '1.1rem' }}
-                                className="
+          <Navbar.Toggle
+            onClick={handleShow}
+            aria-controls="offcanvasNavbar-expand-lg"
+          />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar-expand-lg"
+            aria-labelledby="offcanvasNavbarLabel-expand-lg"
+            placement="end"
+            show={show}
+            onHide={handleClose}
+          >
+            <Offcanvas.Header
+              closeButton
+              onHide={handleClose}
+              className="justify-content-center"
+            >
+              <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
+                Menu
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav
+                style={{ fontSize: "1.1rem" }}
+                className="
                                     ms-auto        /* push to right on large screens */
                                     flex-lg-row    /* horizontal on large screens */
                                     flex-column    /* vertical on small screens */
@@ -46,25 +61,49 @@ const NavigationBar = () => {
                                     align-items-center  /* center align on small */
                                     gap-1
                                 "
-                            >
-                                <Nav.Link onClick={handleClose} as={Link} to="/">Home</Nav.Link>
-                                {/* <Nav.Link onClick={handleClose} href="#about">About</Nav.Link> */}
-                                <Nav.Link onClick={handleClose} as={Link} to="/publications#publications">Publication</Nav.Link>
-                                <Nav.Link onClick={handleClose} as={Link} to="/education#education">Education</Nav.Link>
-                                <Nav.Link onClick={handleClose} as={Link} to="/projects#projects">Projects</Nav.Link>
-                                {/* {user ? (
+              >
+                <NavLink
+                  onClick={handleClose}
+                  to="/"
+                  end
+                  className={styles.navLink}
+                >
+                  Home
+                </NavLink>
+                {/* <Nav.Link onClick={handleClose} href="#about">About</Nav.Link> */}
+                <NavLink
+                  onClick={handleClose}
+                  to="/publications#publications"
+                  className={styles.navLink}
+                >
+                  Publication
+                </NavLink>
+                <NavLink
+                  onClick={handleClose}
+                  to="/education#education"
+                  className={styles.navLink}
+                >
+                  Education
+                </NavLink>
+                <NavLink
+                  onClick={handleClose}
+                  to="/projects#projects"
+                  className={styles.navLink}
+                >
+                  Projects
+                </NavLink>
+                {/* {user ? (
                                     <Nav.Link onClick={handleClose} as={Link} to="/dashboard/profile">{user.user_metadata.full_name || "User"}</Nav.Link>
                                 ) : (
                                     <Nav.Link onClick={handleClose} as={Link} to="/auth/signin">Sign In</Nav.Link>
                                 )} */}
-                            </Nav>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-                </Container>
-            </Navbar>
-
-        </div>
-    );
-}
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
 
 export default NavigationBar;
